7ff5e172d2b855155fa6084208e08da68ecb203d
kayode-adechinan <kayode.adechinan@gmail.com>
2018-11-26 16:30:58 -0500

    aaaaa

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setuptools.setup(
     author_email="kayode.adechinan@gmail.com",
     description="simple python package hosted on Github",
     packages=setuptools.find_packages(),
-    install_requires=[],
     license='MIT',
     zip_safe=False
 )
