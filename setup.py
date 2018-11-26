a554040a9706ac21b930d4c37e132901bcf442cc
kayode-adechinan <kayode.adechinan@gmail.com>
2018-11-26 16:36:27 -0500

    aaaaa

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="owe",
-    version="0.0.1",
+    version="0.0.2",
     author="Kayode Adechinan",
     author_email="kayode.adechinan@gmail.com",
     description="simple python package hosted on Github",
